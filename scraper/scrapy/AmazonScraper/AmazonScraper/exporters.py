e34978639433e129ce77c63882562bf3efdea23d
ADITYAMV1705 <134857206+ADITYAMV1705@users.noreply.github.com>
2024-07-22 22:29:56 +0530

    Delete scraper directory

--- scraper/scrapy/AmazonScraper/AmazonScraper/exporters.py
+++
@@ -1,8 +0,0 @@
-import csv
-from scrapy.exporters import CsvItemExporter
-
-class CustomCsvItemExporter(CsvItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs['quotechar'] = '"'
-        kwargs['quoting'] = csv.QUOTE_ALL
-        super(CustomCsvItemExporter, self).__init__(*args, **kwargs)
